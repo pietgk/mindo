b4d1db372ce35046bdafb82008b15cd7c5929d55
pietgk <piet.groot.kormelink@buddy.inc>
2021-12-12 14:48:35 +0100

    generated to-do screen and components

---
+++ app/components/to-do-list/to-do-list.story.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react-native"
+import { StoryScreen, Story, UseCase } from "../../../storybook/views"
+import { color } from "../../theme"
+import { ToDoList } from "./to-do-list"
+
+storiesOf("ToDoList", module)
+  .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
+  .add("Style Presets", () => (
+    <Story>
+      <UseCase text="Primary" usage="The primary.">
+        <ToDoList style={{ backgroundColor: color.error }} />
+      </UseCase>
+    </Story>
+  ))
